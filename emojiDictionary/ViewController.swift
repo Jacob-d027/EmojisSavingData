ec60bbafec34d564593ad2073fc1a1b31716bb5e
Jacob Davis <jacobdofficial@gmail.com>
2023-10-13 08:03:09 -0600

    Initial Commit

---
+++ emojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emojiDictionary
+//
+//  Created by Jacob Davis on 10/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
